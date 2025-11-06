632c33b02e13ce6775f72d0a95842ca671c65372
Yurii <kukharenko.yura@gmail.com>
2025-11-06 12:32:16 +0200

    Fixed errors connected with mockito dependency.

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -36,10 +36,9 @@ public class MovieDaoImpl implements MovieDao {
 
     @Override
     public Optional<Movie> get(Long id) {
-        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-                Session session = sessionFactory.openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Movie movie = session.get(Movie.class, id);
-            return Optional.of(movie);
+            return Optional.ofNullable(movie);
         } catch (Exception e) {
             throw new DataProcessingException("Can't get movie with id " + id, e);
         }
